6491371adf3c045951942e05091f0eccfbb28105
Mp455 <marcospaulosousa48@gmail.com>
2023-02-22 12:45:19 -0300

    initial commit

---
+++ js/githubUser.js
@@ -0,0 +1,20 @@
+export class githubUser {
+    static search(username){
+        const endpoint = `https://api.github.com/users/${username}`
+    
+        return fetch(endpoint)
+        .then((data) => data.json())
+        .then((data) => {
+            const {login, name, public_repos, followers} = data
+
+            return{
+                login: login,
+                name: name,
+                public_repos: public_repos,
+                followers: followers
+            }
+
+        })
+
+}
+}
